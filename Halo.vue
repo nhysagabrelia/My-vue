2a977c57a9daeb4c6bc7defed8c37826dbd7f25d
nhysagabrelia <150423646+nhysagabrelia@users.noreply.github.com>
2023-11-10 17:31:01 +0800

    Create Halo.vue

---
+++ Halo.vue
@@ -0,0 +1,22 @@
+<!-- Contoh Komponen Vue.js -->
+<template>
+  <div>
+    <h1>{{ greeting }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      greeting: 'Hello, Vue!'
+    };
+  }
+};
+</script>
+
+<style scoped>
+h1 {
+  color: #4caf50;
+}
+</style>
